656438b6e3f4bbb423d0cfa28329f08d81d7912f
AslamThachapalli <aslam.develop912@gmail.com>
2025-03-22 23:34:15 +0530

    coffee mug color change

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -3,7 +3,7 @@ import { Coffee } from "lucide-react";
 export function Attribution() {
     return (
         <div className="absolute bottom-4 w-full flex items-center justify-center">
-            <p className="text-foreground font-semibold">Made with ❤️ and <Coffee className="inline-block w-4 h-4 mb-1 text-amber-950" /> by <a href="https://github.com/AslamThachapalli" target="_blank" rel="noopener noreferrer" className=" text-on-secondary hover:text-tertiary">Aslam Thachapalli</a></p>
+            <p className="text-foreground font-semibold">Made with ❤️ and <Coffee className="inline-block w-4 h-4 mb-1 text-amber-500" /> by <a href="https://github.com/AslamThachapalli" target="_blank" rel="noopener noreferrer" className=" text-on-secondary hover:text-tertiary">Aslam Thachapalli</a></p>
         </div>
     )
 }
